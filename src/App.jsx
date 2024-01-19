0276ec66d7be201b96430eea421c270c78bf5fe2
IsisTD <220126@fppiramide.com>
2024-01-19 12:24:45 +0100

    update

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import FilterableProductTable from './components/FilterableProductTable'
+
+function App() {
+  
+
+  return (
+    <>
+      <FilterableProductTable></FilterableProductTable>
+    </>
+  )
+}
+
+export default App
